5e33288fcc1956d86211d83bbec321e7d1cd9fe4
Lincoln Digby <lincolndigby0@gmail.com>
2024-10-28 20:50:36 -0500

    add append, prepend, tail, size, head, to_s

--- lib/list.rb
+++ lib/list.rb
@@ -11,15 +11,40 @@ class LinkedList
   end
 
   def prepend(value)
+    previous_head = head
+    new_node = Node.new(value, previous_head.next_node)
+
+    @root.next_node = new_node
   end
 
   def size
+    finished = false
+    last_node = @root
+    count = 0
+    until finished
+      break if last_node.next_node.nil?
+
+      count += 1
+      last_node = last_node.next_node
+    end
+    count
   end
 
   def head
+    return nil if @root.next_node.nil?
+
+    @root.next_node
   end
 
   def tail
+    finished = false
+    last_node = @root
+    until finished
+      break if last_node.next_node.nil?
+
+      last_node = last_node.next_node
+    end
+    last_node
   end
 
   def at(index)
@@ -31,6 +56,26 @@ class LinkedList
   def find(value)
   end
 
+  def insert_at(value, index)
+    
+  end
+
+  def remove_at(index)
+
   def to_s
+    finished = false
+    last_node = @root.next_node.nil? ? @root : @root.next_node
+    string_array = []
+    until finished
+      string_array << "(#{last_node.value}) -> "
+
+      if last_node.next_node.nil?
+        string_array << "nil"
+        break
+      end
+
+      last_node = last_node.next_node
+    end
+    string_array.join
   end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
 require_relative "lib/list"
 require_relative "lib/node"
 
+list = LinkedList.new
+
+list.append("dog")
+list.append("cat")
+list.append("parrot")
+list.append("hamster")
+list.append("snake")
+list.append("turtle")
+
+puts list
